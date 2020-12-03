913547ee14c1a44482ed5cea08c16a585405b986
Esther Min <estherjmin@gmail.com>
2020-12-02 20:26:40 -0500

    added comments for instructions for sum-of-nums challenge

--- sum-of-nums.js
+++ sum-of-nums.js
@@ -1,3 +1,16 @@
+// Write a function that takes in an integer and outputs the sum of all the numbers from 1 to that integer.
+
+// Ex:
+// Input: 2
+// Output: 3 (because 1 + 2 = 3)
+
+// Input: 4
+// Output: 10  (because 1 + 2 + 3 + 4 = 10)
+
+// Input: 10
+// Output: 55
+
+
 function addNums (num) {
     let sum = 0;
 
