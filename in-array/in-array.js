b0d35ba613ea7d77110286b89d9015c10abcc233
Esther Min <estherjmin@gmail.com>
2020-12-14 14:47:14 -0500

    solved in-array

---
+++ in-array/in-array.js
@@ -0,0 +1,23 @@
+// Solution 1
+let checkIfIncluded = (arr, el) => {
+    if (arr.includes(el)) {
+        return true;
+    }
+    return false;
+}
+
+// ====================================================
+// Solution 1
+let checkIfIncluded = (arr, el) => {
+    for (let i=0; i < arr.length; i++) {
+        if (arr[i] == el) {
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log(checkIfIncluded([1,2,3], 1)); // Expected output: true
+console.log(checkIfIncluded([1,2,3], 4)); // Expected output: false
+console.log(checkIfIncluded(['code', 'dev', 'nerd'], 'nerd')); // Expected output: true
+console.log(checkIfIncluded(['code', 'dev', 'nerd'], 'genius')); // Expected output: false
